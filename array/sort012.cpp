386ba9af4fa755718bcd17ad05557f57d0073b34
shashwat-code <textshashwat@gmail.com>
2021-01-08 11:25:26 +0530

    sort 0,1,2

---
+++ array/sort012.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<string>
+#include<math.h>
+#include<stack>
+#include<map>
+using namespace std;
+void swap(int* a,int* b){
+	int temp=*a;
+	*a=*b;
+	*b=temp;
+}
+void sort012(int* arr,int n){
+	int i0=0,i1=0,i2=n-1;
+	while(i1<=i2){
+		if(arr[i1]==0){
+			swap(arr+i0,arr+i1);
+			i0++;
+			i1++;
+		}else if(arr[i1]==2){
+			swap(arr[i1],arr[i2]);
+			i2--;
+			i1++;
+		}else{
+			i1++;
+		}
+	}
+}
+int main(){
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++)cin>>arr[i];
+	sort012(arr,n);
+	for(int i=0;i<n;i++)cout<<arr[i]<<" ";
+	cout<<endl;	
+	return 0;
+}
