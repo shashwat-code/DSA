72fa1c3b14628285afd4527c8d9fc0b74333a57f
shashwat-code <textshashwat@gmail.com>
2021-01-09 08:29:41 +0530

    min Jumps

---
+++ array/minJumps.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<string>
+#include<math.h>
+#include<stack>
+#include<map>
+using namespace std;
+
+
+int minJumps(int* arr,int n){
+	int dp[n];
+	dp[0]=0;
+	for(int i=1;i<n;i++){
+		dp[i]=INT_MAX-1;
+	}
+	for(int i=1;i<n;i++){
+		for(int j=0;j<i;j++){
+			if(j+arr[j]>=i){
+				dp[i]=min(dp[i],dp[j]+1);
+			}
+			
+		}
+	}
+	return dp[n-1];
+}
+// Time Complexity: O(n2)
+// Space Complexity: O(n)
+int main(){
+	int arr[] = { 1, 3, 6, 3, 2,
+                  3, 6, 8, 9, 5 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout<<minJumps(arr,n);
+	return 0;
+}
